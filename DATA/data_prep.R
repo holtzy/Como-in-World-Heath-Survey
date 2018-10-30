702f72bcc800300af4f85721ec105ee796b14b26
Holtz Yan <yan.holtz.data@gmail.com>
2018-10-30 15:10:30 +1000

    slowly

---
+++ DATA/data_prep.R
@@ -0,0 +1,9 @@
+# A script that takes data from Carmen and transform it for the web application
+
+# Step 1: compute position for beeswarm plot
+library(beeswarm)
+data <- read.table("data.csv", header=T, sep=",")
+data$pair <- paste(data$Prior_disorder, data$Later_disorder, sep="-")
+beeswarm(log(data$HR), method="center", cex=1, corral="none")
+
+data
