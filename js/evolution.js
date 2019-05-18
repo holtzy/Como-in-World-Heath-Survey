aa151f9d7836a997b2b5aafcd3d359912853c8fe
holtzy <yan.holtz@datadoghq.com>
2019-05-18 11:21:57 +0200

    background of small multiple is ok

---
+++ js/evolution.js
@@ -0,0 +1,41 @@
+// ======================= //
+// SVG AREAS
+// ======================= //
+
+// set the dimensions and margins of the graph
+var margin = {top: 5, right: 5, bottom: 5, left: 5},
+    width = 185 - margin.left - margin.right,
+    height = 185 - margin.top - margin.bottom;
+
+// Remember the list of all disorders is available in allDisorder
+
+// append the svg object to the body of the page
+var svg = d3.select("#dataviz_evolution")
+    .selectAll("uniqueChart")
+    .data(allDisorder)
+    .enter()
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .style("overflow", "visible")
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")" )
+
+// Add background
+svg
+    .append("rect")
+      .attr("x",0)
+      .attr("y",0)
+      .attr("width",width)
+      .attr("height",height)
+      .style("fill", "grey")
+      .style("opacity", .1)
+
+// Add titles to each subplot with disorder Name
+svg
+  .append("text")
+  .attr("text-anchor", "start")
+  .attr("y", 20)
+  .attr("x", 5)
+  .text(function(d){ return( giveShortName(d) )})
+  .style("fill", d => myColor(myGroup(d)) )
--- js/general_variable.js
+++ js/general_variable.js
@@ -15,10 +15,18 @@ var myGroup = d3.scaleOrdinal()
           allGroup[2], allGroup[2], allGroup[2], allGroup[2], allGroup[2], allGroup[2], allGroup[2],
           allGroup[3], allGroup[3], allGroup[3], allGroup[3], allGroup[3]]);
 
+// A scale that returns the short name of a disorder:
+var giveShortName = d3.scaleOrdinal()
+  .domain(allDisorder)
+  .range([ "Depression","Bipolar","Dysthymia",
+                "Panic disorder","Anxiety","Social phobia","Specific phobia","Agoraphobia","PTSD","Obsessive compulsive","Adult separation anxiety","Child separation anxiety",
+                "Anorexia","Bulimia","Binge eating","Intermittent explosive","ADHD","Conduct disorder","Oppositional defiant",
+                "Nicotine dependence","Alcohol abuse","Alcohol dependence","Drug abuse","Drug dependence"]);
+
 // A set of 4 colors:
 var col1 = "#482677FF", col2 = "#2D708EFF", col3 = "#29AF7FFF", col4 = "#B8DE29FF"
 
-// A color scale: group -> color
+// A color scale for groups: group -> color
 var myColor = d3.scaleOrdinal()
   .domain(allGroup)
   .range([col1, col2, col3, col4]);
